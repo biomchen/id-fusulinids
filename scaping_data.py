f08f0ef755b72d04fe235c3b943d9e62e53e55e4
Meng Chen <45435029+biomchen@users.noreply.github.com>
2019-02-20 15:57:41 -0500

    Add some markdown explanation

--- scaping_data.py
+++ scaping_data.py
@@ -8,7 +8,7 @@ data_url = 'https://arxiv.org'
 html =  urlopen(name_url)
 soup =  BeautifulSoup(html)
 
-# scape the faculty names of the EECS department
+# scrape the faculty names of the EECS department
 name_all =  []
 tags =  soup('br')
 
@@ -22,17 +22,17 @@ with open('abstract_data.csv', mode='w') as csvFile:
     writer = csv.DictWriter(csvFile, fieldnames = features)
     writer.writeheader()
 
-    # clea up the faculty names that scaped from MIT website
+    # clean up the faculty names that scaped from MIT website
     for name in name_all:
         name_split =  name.split(' ')
         if len(name_split)!= 2:
             continue
         faculty_name = name_split[1] + '_' + name_split[0]
-
+        
+        # scraping the abstract contnet from abstact links
         search_page  = urlopen(data_url + "/find/all/1/au:+" + faculty_name + "/0/1/0/all/0/1?per_page=50")
         soup = BeautifulSoup(search_page)
-
-        # scaping the abstract contnet from abstact links
+        
         for title in soup(search_page, title = 'Abstract'):
             id =  title.contents[0]
             abstractUrl = data_url + title['href']
